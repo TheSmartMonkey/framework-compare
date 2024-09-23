e166aabf4127c4ef31a9d00a67468dbd340504fc
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2024-09-23 21:57:34 +0200

    add a page

--- nextjs-todo-mongodb/src/app/page.tsx
+++ nextjs-todo-mongodb/src/app/page.tsx
@@ -1,7 +1,3 @@
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      Hello World
-    </div>
-  );
+export default function () {
+  return <div className="flex justify-center items-center h-screen">Hello World</div>;
 }
